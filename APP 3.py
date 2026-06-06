360c512457168ec77afe0798e7b5c5a66ffb2cdf
rubinamin2004-wq <rubinamin2004@gmail.com>
2026-06-06 16:21:12 +0530

    Update APP 3.py

--- APP 3.py
+++ APP 3.py
@@ -368,12 +368,10 @@ with col_left:
     gemini_model = st.selectbox(
         "Gemini Model",
         [
+            "gemini-3.1-flash-lite",
             "gemini-2.5-flash",
-            "gemini-2.5-pro",
-            "gemini-2.0-flash",
-            "gemini-2.0-flash-lite",
-            "gemini-1.5-pro",
-            "gemini-1.5-flash",
+            "gemini-3.5-flash",
+            "gemini-2.5-flash-lite",
         ],
         index=0,
         label_visibility="collapsed",
